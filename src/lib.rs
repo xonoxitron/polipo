0b184e939ae07144310274076d6697b80a891f3a
matteo.pisani <matteo.pisani@retelit.it>
2021-02-21 11:52:31 +0100

    Releasing polipo v0.1.8

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@
 //! Add this to your **```Cargo.toml```**:
 //! ```toml
 //! [dependencies]
-//! polipo = {version = 0.1.7}
+//! polipo = {version = 0.1.8}
 //! ```
 //! 
 //! and then add this to your **```code```**:
